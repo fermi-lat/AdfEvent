005945a6113c08adc18abf92c45dfb3bb7d44b24
ecephas <>
2008-11-07 16:03:00 +0000

    Added the following libraries:
    	     	env.Tool('facilitiesLib')
         		env.Tool('addLibrary', library = env['gaudiLibs'])

--- AdfEventLib.py
+++ AdfEventLib.py
@@ -1,8 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AdfEvent/AdfEventLib.py,v 1.1 2008/08/15 21:42:32 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AdfEvent'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
 
